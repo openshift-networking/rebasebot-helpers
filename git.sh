54c3dd58ed0b4f6cd1c2b9446daaa685ec81fcaf
Mateusz Kowalski <mko@redhat.com>
2025-05-05 09:11:31 +0200

    Update git.sh

--- git.sh
+++ git.sh
@@ -10,4 +10,4 @@ git add .
 
 # When there is nothing to commit, the following will return 1.
 # Because we do not care about no-changes situation, just ignore it.
-git commit -m "UPSTREAM: <carry>: rebasebot: update OCP manifests" || true
+git commit -m "UPSTREAM: <carry>: rebasebot: update OCP manifests and fix golang deps" || true
